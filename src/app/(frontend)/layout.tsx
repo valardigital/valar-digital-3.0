65392229da20c47974f4ec8655a37cd19b63fe72
utkarsh-valardigital <utkarsh@valardigital.com>
2025-11-24 13:33:08 +0530

    Add LinkedIn Insight Tag for enhanced tracking and analytics in layout component

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import Header from './components/layout/header';
-import Footer from './components/layout/Footer';
+import Script from "next/script";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/Footer";
 import { LivePreviewListener } from "@/components/LivePreviewListener";
 import localFont from "next/font/local";
 import "slick-carousel/slick/slick.css";
@@ -57,6 +58,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${neueMontreal.variable} antialiased font-sans`}>
+        <Script id="linkedin-insight-tag" strategy="afterInteractive">
+          {`_linkedin_partner_id = "9044801";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+(function (l) {
+  if (!l) {
+    window.lintrk = function (a, b) {
+      window.lintrk.q.push([a, b]);
+    };
+    window.lintrk.q = [];
+  }
+  var s = document.getElementsByTagName("script")[0];
+  var b = document.createElement("script");
+  b.type = "text/javascript";
+  b.async = true;
+  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+  s.parentNode.insertBefore(b, s);
+})(window.lintrk);`}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=9044801&fmt=gif" />',
+          }}
+        />
         <LivePreviewListener />
         <Header />
         {children}
